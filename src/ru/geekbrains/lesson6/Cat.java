176dcc9c6e2484760965e9b26fb2a6c82955132a
Lianna1990 <egoian.lianna@mail.ru>
2019-06-04 13:34:34 +0300

    initial commit

---
+++ src/ru/geekbrains/lesson6/Cat.java
@@ -0,0 +1,18 @@
+package ru.geekbrains.lesson6;
+
+public class Cat {
+    public String name;
+    public int age;
+    public int id;
+
+    public  Cat(){
+
+    }
+
+    public  Cat(String name, int age, int id){
+        this.name = name;
+        this.age = age;
+        this.id = id;
+
+    }
+}
---
+++ src/ru/geekbrains/lesson6/Node.java
@@ -0,0 +1,33 @@
+package ru.geekbrains.lesson6;
+
+public class Node {
+    public Cat cat;
+    public Node leftChild;
+    public Node rightChild;
+
+    public void display() {
+        System.out.println ("Name: " +cat.name+", age: "+cat.age);
+    }
+
+   /*public Node getSuccessor(Node node) {
+        Node successorParent = node;
+        Node successor = node;
+        Node currant = node.rightChild;
+
+        while (currant != null) {
+            successorParent = successor;
+            successor = currant;
+            currant = currant.leftChild;
+        }
+
+        if (successor != node.rightChild) {
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = node.rightChild;
+        }
+
+        return successor;
+    }*/
+}
+
+
+
